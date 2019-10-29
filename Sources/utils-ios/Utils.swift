5fa71d8d15dc9537ffe2b762181f219b517c3df2
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2019-10-29 15:16:12 +0200

    add RxSwift dep

--- Sources/utils-ios/Utils.swift
+++ Sources/utils-ios/Utils.swift
@@ -1,11 +1,13 @@
 //
-//  File.swift
+//  Utils.swift
 //  
 //
 //  Created by Miroslav Yozov on 29.10.19.
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 public struct Utils {
     private init() { }

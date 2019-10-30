4a5cfebf8ea6b92784aa3e04f7b3fd04c488fa93
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2019-10-30 13:07:57 +0200

    fix & test

--- Sources/utils-ios/AssociatedObjectCompatible.swift
+++ Sources/utils-ios/AssociatedObjectCompatible.swift
@@ -46,7 +46,7 @@ public protocol AssociatedObjectCompatible: Synchronized {
 public extension AssociatedObjectCompatible {
     func get<T>(for key: String, default value: T) -> T {
         return synchronized {
-            if let current = objc_getAssociatedObject(self, key) as? T {
+            if let current = objc_getAssociatedObject(self, AssociatedKey.pointer(for: key)) as? T {
                 return current
             }
             

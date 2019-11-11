e072d728753bd88a58f00ba80051d2b176ba7da8
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2019-11-11 13:46:43 +0200

    Remote Container

--- Sources/utils-ios/Entities/Container+RxRemoteCompatible.swift
+++ Sources/utils-ios/Entities/Container+RxRemoteCompatible.swift
@@ -12,19 +12,30 @@ public class RemoteContainer<Element: Decodable, EndpointType: EndpointPageble>:
     RxRemoteCompatible,
     RxRemotePageCompatible {
     
-    let endpoint: EndpointType
+    // MAK: RxRemoteCompatible, RxRemotePageCompatible
+    public let remoteEndpoint: EndpointType
+    public private (set) var defaultRemoteState: RemoteState
+    public private (set) var remoteHasNextPage = true
     
     public required init(from decoder: Decoder) throws {
-        self.endpoint = try decoder.endpoint()
+        self.remoteEndpoint = try decoder.endpoint()
+        self.defaultRemoteState = .done
         try super.init(from: decoder)
     }
     
-    public var remoteEndpoint: EndpointType {
-        return endpoint
+    public init(endpoint: EndpointType, elements: [Element] = []) {
+        self.remoteEndpoint = endpoint
+        self.defaultRemoteState = .done
+        super.init(elements: elements)
     }
     
-    public var remoteHasNextPage: Bool {
-        return false
+    override public func decode(from decoder: Decoder) throws {
+        let decodeType = decoder.decodeType
+        let currentCount = count
+        
+        try super.decode(from: decoder)
+        
+        remoteHasNextPage = decodeType == .replace ? true : currentCount < count
     }
     
     deinit {

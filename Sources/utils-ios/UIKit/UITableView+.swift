85361805d323338f0aa0c642881a09a6ede092fd
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2019-11-28 17:30:17 +0200

    UITableView+

---
+++ Sources/utils-ios/UIKit/UITableView+.swift
@@ -0,0 +1,18 @@
+//
+//  UITableView+.swift
+//  
+//
+//  Created by Miroslav Yozov on 28.11.19.
+//
+
+import UIKit
+
+public extension UITableView {
+    func register<T: UITableViewCell>(_ cellClass: T.Type) {
+        register(cellClass.nib, forCellReuseIdentifier: cellClass.nibName)
+    }
+
+    func dequeueReusableCell<T: UITableViewCell>(_ cellClass: T.Type = T.self, for indexPath: IndexPath) -> T {
+        return dequeueReusableCell(withIdentifier: cellClass.nibName, for: indexPath) as! T
+    }
+}

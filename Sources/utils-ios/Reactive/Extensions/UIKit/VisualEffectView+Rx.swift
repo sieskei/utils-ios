9ef531b537479ac300ec72caa695fb0ee5764355
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2019-12-03 09:34:29 +0200

    VisualEffectView

---
+++ Sources/utils-ios/Reactive/Extensions/UIKit/VisualEffectView+Rx.swift
@@ -0,0 +1,35 @@
+//
+//  VisualEffectView+Rx.swift
+//  sinoptik-ios
+//
+//  Created by Miroslav Yozov on 7.06.19.
+//  Copyright © 2019 Net Info. All rights reserved.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: VisualEffectView {
+    /// Bindable sink for `colorTint` property.
+    var colorTint: Binder<UIColor?> {
+        return Binder(self.base) { view, color in
+            view.colorTint = color
+        }
+    }
+    
+    /// Bindable sink for `colorTintAlpha` property.
+    var colorTintAlpha: Binder<CGFloat> {
+        return Binder(self.base) { view, alpha in
+            view.colorTintAlpha = alpha
+        }
+    }
+    
+    /// Bindable sink for `blurRadius` property.
+    var blurRadius: Binder<CGFloat> {
+        return Binder(self.base) { view, radius in
+            view.blurRadius = radius
+        }
+    }
+}

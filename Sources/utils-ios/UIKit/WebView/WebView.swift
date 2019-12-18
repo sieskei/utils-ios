0e61c95fa56254d90af9417632f4657ec47b1c63
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2019-12-18 14:18:35 +0200

    WebView: header view support

--- Sources/utils-ios/UIKit/WebView/WebView.swift
+++ Sources/utils-ios/UIKit/WebView/WebView.swift
@@ -8,12 +8,76 @@
 import UIKit
 import WebKit
 
+import RxSwift
+import RxCocoa
+
+import Material
+
 public protocol WebViewUIDelegate: WKUIDelegate {
     func webView(_ webView: WebView, didUpdate contentSize: CGSize)
 }
 
 open class WebView: WKWebView {
-    private var KVOContext = 0
+    private let disposeBag = DisposeBag()
+    private var headerViewBoundsDisposable: Disposable? {
+        didSet {
+            oldValue?.dispose()
+            headerViewBoundsDisposable?.disposed(by: disposeBag)
+        }
+    }
+    
+    var headerView: UIView? {
+        didSet {
+            guard headerView != oldValue else { return }
+        
+            if let view = oldValue {
+                headerViewBoundsDisposable = nil
+                scrollView.contentInset.top -= view.bounds.height
+            }
+            
+            if let view = headerView {
+                defer {
+                    layoutHeader()
+                }
+                
+                headerViewBoundsDisposable = view.rx.observeWeakly(CGRect.self, #keyPath(UIView.bounds))
+                    .map { $0?.height ?? 0 }
+                    .distinctUntilChanged()
+                    .subscribeNextWeakly(weak: scrollView) { scrollView, height in
+                        guard scrollView.contentOffset.y + scrollView.contentInset.top >= 0 else {
+                             return
+                        }
+
+                        let diff = height - scrollView.contentInset.top
+                        scrollView.contentInset.top += diff
+                    }
+            }
+        }
+    }
+    
+    public private (set) lazy var scrollPositionMarkers: (top: UIView, bottom: UIView) = {
+        let source = scrollView.rx.contentOffset.map { $0.y }.distinctUntilChanged()
+        
+        // top
+        let topView = UIView()
+        topView.backgroundColor = .clear
+        source.subscribeNextWeakly(weak: topView) {
+            if $0.superview != nil {
+                $0.layout.top(-$1)
+            }
+        }.disposed(by: disposeBag)
+        
+        // bottom
+        let bottomView = UIView()
+        bottomView.backgroundColor = .clear
+        source.subscribeNextWeakly(weaks: bottomView, scrollView) {
+            if $0.superview != nil {
+                $0.layout.top($1.contentSize.height - $2)
+            }
+        }.disposed(by: disposeBag)
+        
+        return (topView, bottomView)
+    }()
     
     public convenience init(configuration: WKWebViewConfiguration = WKWebViewConfiguration()) {
         configuration.websiteDataStore = WKWebsiteDataStore.nonPersistent()
@@ -37,6 +101,8 @@ open class WebView: WKWebView {
     }
     
     open func prepare() {
+        scrollView.backgroundColor = .clear
+        
         let handler = ContentHeightMessageHandler(webView: self)
         configuration.userContentController.add(handler, name: ContentHeightMessageHandler.name)
         
@@ -60,6 +126,57 @@ open class WebView: WKWebView {
         
         let heightListenerScript = WKUserScript(source: heightListenerSource, injectionTime: WKUserScriptInjectionTime.atDocumentEnd, forMainFrameOnly: true)
         configuration.userContentController.addUserScript(heightListenerScript)
+    }
+    
+    override open func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        layoutHeader()
+    }
+    
+    private func prepareForLayout(_ view: UIView, _ layouter: (Layout) -> Void) {
+        guard let superview = superview else  {
+            return
+        }
+        
+        if view.superview != superview {
+            view.removeFromSuperview()
+            
+            view.translatesAutoresizingMaskIntoConstraints = false
+            superview.insertSubview(view, belowSubview: self)
+        }
+        
+        layouter(view.layout)
+    }
+    
+    private func layoutHeader() {
+        guard let view = headerView else {
+            return
+        }
+        
+        layoutMarkers()
+        
+        prepareForLayout(view) {
+            $0.left(self)
+                .right(self)
+                .top(self, 0, <=)
+                .bottom(scrollPositionMarkers.top.anchor.top)
+        }
+    }
+    
+    private func layoutMarkers() {
+        prepareForLayout(scrollPositionMarkers.top) {
+            $0.left(self)
+                .right(self)
+                .top(-scrollView.contentOffset.y)
+                .height(0)
+        }
+        
+        prepareForLayout(scrollPositionMarkers.bottom) {
+            $0.left(self)
+                .right(self)
+                .top(-scrollView.contentSize.height)
+                .height(0)
+        }
     }
 }
 

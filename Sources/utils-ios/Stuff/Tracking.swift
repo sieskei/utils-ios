195d1cc965b1a597f1b29ab52d8fb570d40df06b
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2020-01-21 16:53:17 +0200

    add tracking

---
+++ Sources/utils-ios/Stuff/Tracking.swift
@@ -0,0 +1,48 @@
+//
+//  Tracking.swift
+//  
+//
+//  Created by Miroslav Yozov on 21.01.20.
+//
+
+import Foundation
+
+public protocol TrackCompatibleSource {
+    var trackingScreen: TrackCompatibleItem { get }
+}
+
+public protocol TrackCompatibleItem {
+    typealias Parameters = [String: Any]
+    
+    var params: Parameters { get }
+}
+
+public protocol TrackCompatibleEvent: TrackCompatibleItem {
+    var eventName: String { get }
+}
+
+public protocol TrackCompatible {
+    func initialize()
+    func track(params: TrackCompatibleItem.Parameters)
+}
+
+public struct TrackSystems {
+    private static var instances: [TrackCompatible] = {
+        return []
+    }()
+    
+    static func initialize() {
+        instances.forEach {
+            $0.initialize()
+        }
+    }
+    
+    static func track(meta: TrackCompatibleEvent) {
+        var params = meta.params
+        params["event"] = meta.eventName
+        
+        instances.forEach {
+            $0.track(params: params)
+        }
+    }
+}

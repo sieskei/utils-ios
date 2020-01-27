235807682feae9c7617cc5b717925b29666f411a
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2020-01-27 08:56:29 +0200

    WebView set header frame with bounds

--- Sources/utils-ios/UIKit/WebView/WebView.swift
+++ Sources/utils-ios/UIKit/WebView/WebView.swift
@@ -18,7 +18,7 @@ open class WebView: WKWebView {
     public let headerBoundsPauser: EquatableValue<Bool> = .init(true)
     
     public private (set) lazy var headerContainerView: UIView = {
-        let view = UIView()
+        let view = UIView(frame: .init(origin: .zero, size: .init(width: bounds.width, height: 0)))
         view.backgroundColor = .clear
         return view
     }()

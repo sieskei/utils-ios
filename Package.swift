e4628259543921be07bc7363302fca3e21e2641b
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2020-02-24 14:13:15 +0200

    material/motion commit

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         .package(url: "https://github.com/RxSwiftCommunity/RxSwiftExt.git", from: "5.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.0"),
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "3.5.0"),
-        .package(url: "ssh://git@repos.ni.bg/nidata/git/CosmicMind/Material.git", .branch("development")),
-        .package(url: "ssh://git@repos.ni.bg/nidata/git/CosmicMind/Motion.git", .branch("development"))
+        .package(url: "ssh://git@repos.ni.bg/nidata/git/CosmicMind/Material.git", .revision("52c76a07a82cdc61494bf9e6871dbf99e4685cf7")),
+        .package(url: "ssh://git@repos.ni.bg/nidata/git/CosmicMind/Motion.git", .revision("808ac0cf1b67c5e4163ee867dd6c056836fcbc58")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

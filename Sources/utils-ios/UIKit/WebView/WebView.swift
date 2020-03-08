73d6da6645f1e2de381551d958e79da753b17db4
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2020-03-08 19:25:53 +0200

    WebView updates.

--- Sources/utils-ios/UIKit/WebView/WebView.swift
+++ Sources/utils-ios/UIKit/WebView/WebView.swift
@@ -29,6 +29,26 @@ open class WebView: WKWebView {
         return view
     }()
     
+    open var headerTopConstraintConstant: Observable<CGFloat> {
+        // return scrollView.rx.contentOffset.map { -max(0, $0.y ) }
+        return scrollView.rx.contentOffset.map { -$0.y }
+    }
+    
+    open var footerTopConstraintConstant: Observable<CGFloat> {
+        /*
+         View height propery
+         */
+        let vh = footerContainerView.rx.observeWeakly(CGRect.self, #keyPath(UIView.bounds)).map { $0?.height ?? 0 }
+        
+        /*
+         Scroll view content offset and size properties.
+         */
+        let cs = scrollView.rx.observeWeakly(CGSize.self,  #keyPath(UIScrollView.contentSize)).map   { $0?.height ?? 0 }
+        let co = scrollView.rx.observeWeakly(CGPoint.self, #keyPath(UIScrollView.contentOffset)).map { $0?.y      ?? 0 }
+        
+        return Observable.combineLatest(vh, cs, co).map { ($1 - $0) - $2 }
+    }
+    
     public convenience init(configuration: WKWebViewConfiguration = WKWebViewConfiguration()) {
         configuration.websiteDataStore = WKWebsiteDataStore.nonPersistent()
         self.init(frame: .zero, configuration: configuration)
@@ -55,9 +75,10 @@ open class WebView: WKWebView {
         backgroundColor = .clear
         scrollView.backgroundColor = .clear
         
+        prepareConfiguration()
+        
         prepareHeaderContainerView()
         prepareFooterContainerView()
-        prepareBodyInsets()
     }
     
     open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
@@ -68,24 +89,6 @@ open class WebView: WKWebView {
 }
 
 fileprivate extension WebView {
-    enum Margin: Equatable {
-        case top(CGFloat)
-        case bottom(CGFloat)
-        
-        var script: String {
-            switch self {
-            case .top(let v):
-                return "document.body.style.marginTop = \"\(v)px\""
-            case .bottom(let v):
-                return "document.body.style.marginBottom = \"\(v)px\""
-            }
-        }
-    }
-    
-    private func set(bodyMargin margin: Margin) {
-        evaluateJavaScript(margin.script)
-    }
-    
     func prepareHeaderContainerView() {
         let view = headerContainerView
         
@@ -94,7 +97,7 @@ fileprivate extension WebView {
         
         // dynamic top constraint
         let topConstraint = view.topAnchor.constraint(equalTo: topAnchor)
-        scrollView.rx.contentOffset.map { -max(0, $0.y ) }.bind(to: topConstraint.rx.constant).disposed(by: disposeBag)
+        headerTopConstraintConstant.distinctUntilChanged().bind(to: topConstraint.rx.constant).disposed(by: disposeBag)
         
         // static constraints
         NSLayoutConstraint.activate([
@@ -110,14 +113,10 @@ fileprivate extension WebView {
         view.translatesAutoresizingMaskIntoConstraints = false
         scrollView.insertSubview(view, at: 1)
         
-        let vh = view.rx.observeWeakly(CGRect.self, #keyPath(UIView.bounds)).map { $0?.height ?? 0 }
-        let cs = scrollView.rx.observeWeakly(CGSize.self, #keyPath(UIScrollView.contentSize)).map { $0?.height ?? 0 }
-        let co = scrollView.rx.observeWeakly(CGPoint.self, #keyPath(UIScrollView.contentOffset)).map { $0?.y ?? 0 }
-        
         // dynamic top constraint
         let topConstraint = view.topAnchor.constraint(equalTo: topAnchor)
-        Observable.combineLatest(vh, cs, co).map { ($1 - $0) - $2 }.distinctUntilChanged().bind(to: topConstraint.rx.constant).disposed(by: disposeBag)
-
+        footerTopConstraintConstant.distinctUntilChanged().bind(to: topConstraint.rx.constant).disposed(by: disposeBag)
+        
         // static constraints
         NSLayoutConstraint.activate([
             topConstraint,
@@ -125,28 +124,61 @@ fileprivate extension WebView {
             view.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
     }
-    
-    func prepareBodyInsets() {
-        typealias O = Observable<Margin>
+}
+
+
+// -----------------------------------
+// MARK: Document end message handler.
+// -----------------------------------
+extension WebView: WKScriptMessageHandler {
+    private enum Margin: Equatable {
+        case top(CGFloat)
+        case bottom(CGFloat)
         
+        var script: String {
+            switch self {
+            case .top(let v):
+                return "document.body.style.marginTop = \"\(v)px\""
+            case .bottom(let v):
+                return "document.body.style.marginBottom = \"\(v)px\""
+            }
+        }
+    }
+    
+    private static var messageName = "documentEnd"
+    
+    internal func prepareConfiguration() {
+        let ucc = configuration.userContentController
+        ucc.addUserScript(.init(source: "webkit.messageHandlers.documentEnd.postMessage(\"\");",
+                                injectionTime: .atDocumentEnd, forMainFrameOnly: true))
+        ucc.add(self, name: WebView.messageName)
+    }
+    
+    private func set(bodyMargin margin: Margin) {
+        evaluateJavaScript(margin.script)
+    }
+    
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        guard message.webView == self, message.name == WebView.messageName else {
+            return
+        }
+        
+        typealias O = Observable<Margin>
+                
         let hv = headerContainerView
         let fv = footerContainerView
-        
-        let l = rx.observeWeakly(Bool.self, #keyPath(WKWebView.isLoading), options: [.new]).map { $0 ?? false }.filter { !$0 }
         
         /*
          Top inset (margin) based on header view height.
         */
-        let lh: O = l.pausableBuffered(headerBoundsPauser, limit: nil).mapWeakly(weak: hv, default: .top(0)) { v, _ in .top(v.bounds.height) }
         let hh: O = hv.rx.observeWeakly(CGRect.self, #keyPath(UIView.bounds)).pausable(headerBoundsPauser).map { .top($0?.height ?? 0) }
         
         /*
          Bottom inset (margin) based on footer view height.
         */
-        let lf: O = l.mapWeakly(weak: fv, default: .bottom(0)) { v, _ in .bottom(v.bounds.height) }
         let hf: O = fv.rx.observeWeakly(CGRect.self, #keyPath(UIView.bounds)).map { .bottom($0?.height ?? 0) }
         
-        Observable.merge(lh, hh, lf, hf).distinctUntilChanged().subscribeNextWeakly(weak: self) {
+        Observable.merge(hh, hf).subscribeNextWeakly(weak: self) {
             $0.set(bodyMargin: $1)
         }.disposed(by: disposeBag)
     }

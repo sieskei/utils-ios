f4f894c427d72e2e0cbb996157fb960426a8c147
Yonita Petrova <yonita.petrova@netinfo.bg>
2020-08-24 17:07:26 +0300

    Collection view - deque reusable cell with addToSubviews

--- Sources/utils-ios/UIKit/UICollectionView+.swift
+++ Sources/utils-ios/UIKit/UICollectionView+.swift
@@ -11,12 +11,16 @@ public extension UICollectionView {
     func register<T: UICollectionViewCell>(_ cellClass: T.Type) {
         register(cellClass, forCellWithReuseIdentifier: cellClass.className)
     }
-
-    func dequeueReusableCell<T: UICollectionViewCell>(_ cellClass: T.Type = T.self, for indexPath: IndexPath) -> T {
-        return dequeueReusableCell(withReuseIdentifier: cellClass.className, for: indexPath) as! T
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(_ cellClass: T.Type = T.self, for indexPath: IndexPath, addToSubviews flag: Bool = false) -> T {
+        let cell: T = dequeueReusableCell(withReuseIdentifier: cellClass.className, for: indexPath) as! T
+        if flag, !cell.is(childOf: self) {
+            addSubview(cell)
+        }
+        return cell
     }
     
-    func dequeueReusableCell<T: UICollectionViewCell>(_ cellClass: T.Type = T.self, for row: Int) -> T {
-        return dequeueReusableCell(for: .init(row: row, section: 0))
+    func dequeueReusableCell<T: UICollectionViewCell>(_ cellClass: T.Type = T.self, for row: Int, addToSubviews flag: Bool = false) -> T {
+        return dequeueReusableCell(for: .init(row: row, section: 0), addToSubviews: flag)
     }
 }

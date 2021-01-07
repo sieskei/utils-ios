5b3a2ee85fc0846bb7bc94866dca883b575a40df
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2021-01-08 00:53:54 +0200

    UILabel+ revert

--- Sources/utils-ios/UIKit/Ext/UILabel+.swift
+++ Sources/utils-ios/UIKit/Ext/UILabel+.swift
@@ -15,15 +15,15 @@ public extension UILabel {
         }
         
         let source: String
-//        if let font = font {
-//            let color = textColor?.hexString(.RRGGBB) ?? "#000000"
-//            source =
-//            """
-//            <span style=\"display: block; font-family: '\(font.fontName)'; font-size: \(font.pointSize)px; color: \(color); text-align: \(textAlignment.description);\">\(text.trimmingCharacters(in: .whitespacesAndNewlines))</span>
-//            """
-//        } else {
+        if let font = font {
+            let color = textColor?.hexString(.RRGGBB) ?? "#000000"
+            source =
+            """
+            <span style=\"display: block; font-family: '\(font.fontName)'; font-size: \(font.pointSize)px; color: \(color); text-align: \(textAlignment.description);\">\(text.trimmingCharacters(in: .whitespacesAndNewlines))</span>
+            """
+        } else {
             source = text
-//        }
+        }
         
         if let data = source.data(using: String.Encoding.utf16),
            let at = try? NSMutableAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {

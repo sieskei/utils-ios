9b4cc289acad10b136960b5ea65248af50b6cb49
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2021-04-19 14:39:58 +0300

    Tracking updates

--- Sources/utils-ios/Utils+Tracking.swift
+++ Sources/utils-ios/Utils+Tracking.swift
@@ -9,8 +9,32 @@ import Foundation
 import AppTrackingTransparency
 import RxSwift
 
+public protocol TrackingItem {
+    typealias Parameters = [String: Any]
+    var params: Parameters { get }
+}
+
+public protocol TrackingPlatform {
+    func initialize()
+    func track(item: TrackingItem)
+}
+
 extension Utils {
     public struct Tracking: ReactiveCompatible {
+        enum Initialized {
+            case `true`
+            case `false`
+            case running
+            
+            var asBool: Bool { self == .true }
+            var isRunning: Bool { self == .running }
+        }
+        
+        private static let disposeBag: DisposeBag = .init()
+        
+        @RxProperty
+        private static var initialized: Initialized = .false
+        
         @RxProperty
         fileprivate static var isRequestedProperty: Bool = {
             guard #available(iOS 14, *) else {
@@ -30,14 +54,72 @@ extension Utils {
             }
         }()
         
+        private static var trackQueue: Notifier<TrackingItem> = {
+            let queue: Notifier<TrackingItem> = .init()
+            
+            queue
+                .pausableBuffered($initialized.value.map { $0.asBool }, limit: nil)
+                .subscribe(onNext: { i in
+                    platforms.forEach {
+                        $0.track(item: i)
+                    }
+                }).disposed(by: disposeBag)
+            
+            return queue
+        }()
+        
+        private static var platforms: [TrackingPlatform] = []
+        
         public static var isRequested: Bool {
             isRequestedProperty
+        }
+        
+        public static func register(platforms: TrackingPlatform...) {
+            guard !initialized.asBool else {
+                Utils.Log.error("Tracking platform must be register before initialization.")
+                return
+            }
+            
+            platforms.forEach {
+                Tracking.platforms.append($0)
+            }
+        }
+        
+        public static func initialize() {
+            guard !initialized.asBool else {
+                Utils.Log.warning("Already initialized.")
+                return
+            }
+            
+            guard !initialized.isRunning else {
+                return
+            }
+            
+            rx.isRequested
+                .do(onCompleted: {
+                    platforms.forEach {
+                        $0.initialize()
+                    }
+                    initialized = .true
+                }, onSubscribe: {
+                    initialized = .running
+                })
+                .subscribe()
+                .disposed(by: disposeBag)
+        }
+        
+        public static func track(item: TrackingItem) {
+            trackQueue.notify(item)
         }
     }
 }
 
 extension Reactive where Base == Utils.Tracking {
     public static var isRequested: Observable<Bool> {
-        Base.$isRequestedProperty.value.asObservable()
+        Base.$isRequestedProperty.value
+            .asObservable()
+            .skip(while: { !$0 })
+            .take(1)
+            .observe(on: MainScheduler.instance)
     }
 }

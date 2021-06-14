3bfd286c8ef0a8cb079e7efd63df3918eda5722a
Miroslav Yozov <miroslav.yozov@netinfo.bg>
2021-06-14 14:24:49 +0300

    RxCoordinator updates

--- Sources/utils-ios/UIKit/Coordinator/RxCoordinator.swift
+++ Sources/utils-ios/UIKit/Coordinator/RxCoordinator.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 import RxSwift
+import RxSwiftExt
 
 /// Base abstract coordinator generic over the return type of the `start` method.
 open class RxCoordinator<ResultType> {
     /// Typealias which will allows to access a ResultType of the Coordainator by `CoordinatorName.CoordinationResult`.
     public typealias CoordinationResult = ResultType
+    
+    /// Typealias for controller and job events.
+    public typealias CoordinationStart = (controller: UIViewController, events: Observable<Event>)
+    
+    /// Coordinator's life cycle.
+    public enum LifeCycle {
+        case present(UIViewController)
+        case event(ResultType)
+        case dismiss(UIViewController)
+    }
+    
+    /// Coordinator's job events.
+    public enum Event {
+        case event(ResultType)
+        case dismiss
+    }
     
     /// Utility `DisposeBag` used by the subclasses.
     public let disposeBag = DisposeBag()
@@ -40,27 +57,98 @@ open class RxCoordinator<ResultType> {
     private func free<T>(coordinator: RxCoordinator<T>) {
         childCoordinators[coordinator.identifier] = nil
     }
-
-    /// 1. Calls method `start()` on that coordinator.
-    /// 2. On the `onSubscribe:` of returning observable of method `start()` stores coordinator in a dictionary of child coordinators.
-    /// 3. On the `afterCompleted:` of returning observable of method `start()` removes coordinator from the dictionary.
+    
+    /// Calls method `start()` on that coordinator.
     ///
     /// - Parameter coordinator: Coordinator to start.
     /// - Returns: Result of `start()` method.
-    public func coordinate<T>(to coordinator: RxCoordinator<T>) -> Observable<T> {
-        coordinator
-            .start()
-            .do(weak: self, afterCompleted: {
-                $0.free(coordinator: coordinator)
-            }, onSubscribe: {
-                $0.store(coordinator: coordinator)
+    public func move<T>(to coordinator: RxCoordinator<T>) -> Observable<RxCoordinator<T>.LifeCycle> {
+        let start: RxCoordinator<T>.CoordinationStart = coordinator.start()
+        return start.events
+            .take(until: { $0.isDismiss }, behavior: .inclusive)
+            .map {
+                switch $0 {
+                case .event(let r):
+                    return .event(r)
+                case .dismiss:
+                    return .dismiss(start.controller)
+                }
+            }
+            .startWith(.present(start.controller))
+            .do(weak: self, onCompleted: { this in
+                this.free(coordinator: coordinator)
+            }, onSubscribe: { this in
+                this.store(coordinator: coordinator)
             })
     }
     
     /// Starts job of the coordinator.
     ///
-    /// - Returns: Result of coordinator job.
-    open func start() -> Observable<ResultType> {
+    /// - Returns: Controller and events of coordinator job.
+    open func start() -> CoordinationStart {
         fatalError("Start method should be implemented.")
+    }
+}
+
+/// Coordinator life cycle utilities.
+public extension RxCoordinator.LifeCycle {
+    var isPresent: Bool {
+        switch self {
+        case .present:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    var mapToEvent: FilterMap<RxCoordinator<ResultType>.Event> {
+        switch self {
+        case .event(let r):
+            return .map(.event(r))
+        default:
+            return .ignore
+        }
+    }
+    
+    func on(present call: (UIViewController) -> Void) {
+        if case .present(let vc) = self {
+            call(vc)
+        }
+    }
+    
+    func on(event call: (ResultType) -> Void) {
+        if case .event(let r) = self {
+            call(r)
+        }
+    }
+    
+    func on(dismiss call: (UIViewController) -> Void) {
+        if case .dismiss(let vc) = self {
+            call(vc)
+        }
+    }
+}
+
+/// Coordinator job event utilities.
+public extension RxCoordinator.Event {
+    var isDismiss: Bool {
+        switch self {
+        case .dismiss:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    func on(event call: (ResultType) -> Void) {
+        if case .event(let r) = self {
+            call(r)
+        }
+    }
+    
+    func on(dismiss call: () -> Void) {
+        if case .dismiss = self {
+            call()
+        }
     }
 }

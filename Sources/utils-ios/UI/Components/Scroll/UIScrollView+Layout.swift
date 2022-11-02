e1f638518f1eb07c22f8c10d2f68b36760a067b0
Miroslav Yozov <miro.yozov@gmail.com>
2022-11-02 16:37:43 +0200

    UIScrollView layout updates

--- Sources/utils-ios/UI/Components/Scroll/UIScrollView+Layout.swift
+++ Sources/utils-ios/UI/Components/Scroll/UIScrollView+Layout.swift
@@ -23,107 +23,100 @@ public extension UIScrollView {
     
     convenience init(elements: [Element], direction: UIScrollView.Direction = .vertical) {
         self.init(frame: .zero)
-        set1(elements: elements, direction: direction)
+        set(elements: elements, direction: direction)
     }
     
-    func set1(views: [UIView], insets: UIEdgeInsets = .zero, direction: UIScrollView.Direction = .vertical) {
-        set1(elements: views.map { ($0, insets) }, direction: direction)
+    func set(views: [UIView], insets: UIEdgeInsets = .zero, direction: UIScrollView.Direction = .vertical) {
+        set(elements: views.map { ($0, insets) }, direction: direction)
     }
     
-    func set1(elements: [Element], direction: UIScrollView.Direction = .vertical) {
-        let frameAnchors: UtilsUIAnchorsCompatible
-        let contentAnchros: UtilsUIAnchorsCompatible
+    func set(elements elems: [Element], direction: UIScrollView.Direction = .vertical) {
+        let frame: UtilsUIAnchorsCompatible
+        let content: UtilsUIAnchorsCompatible
         let parent: UIView
         
-//        if #available(iOS 11, *) {
-//            parent = self
-//            frameAnchors = frameLayoutGuide
-//            contentAnchros = contentLayoutGuide
-//        } else {
+        if #available(iOS 11, *) {
+            parent = self
+            frame = frameLayoutGuide
+            content = contentLayoutGuide
+        } else {
             // allways create new one
             let container: UIView = container(direction: direction)
             parent = container
-            frameAnchors = self
-            contentAnchros = container
-//        }
-        
-        /*
-        parent.subviews.forEach {
-            $0.removeFromSuperview()
+            frame = self
+            content = container
         }
-        */
         
-        
-        guard !elements.isEmpty else {
+        guard elements.swap(with: elems) else {
             return
         }
         
-        elements.forEach { e in
+        elems.forEach { e in
             e.view.translatesAutoresizingMaskIntoConstraints = false
             parent.addSubview(e.view)
             
             switch direction {
             case .vertical:
-                e.view.leftAnchor == contentAnchros.leftAnchor + e.insets.left
-                e.view.rightAnchor == contentAnchros.rightAnchor - e.insets.right
-                e.view.widthAnchor == frameAnchors.widthAnchor - e.insets.horizontal
+                e.view.leftAnchor == content.leftAnchor + e.insets.left
+                e.view.rightAnchor == content.rightAnchor - e.insets.right
+                e.view.widthAnchor == frame.widthAnchor - e.insets.horizontal
             case .horizontal:
-                e.view.topAnchor == contentAnchros.topAnchor + e.insets.top
-                e.view.bottomAnchor == contentAnchros.bottomAnchor - e.insets.bottom
-                e.view.heightAnchor == frameAnchors.heightAnchor - e.insets.vertical
+                e.view.topAnchor == content.topAnchor + e.insets.top
+                e.view.bottomAnchor == content.bottomAnchor - e.insets.bottom
+                e.view.heightAnchor == frame.heightAnchor - e.insets.vertical
             }
         }
         
         if direction == .horizontal, isPagingEnabled {
-            elements.forEach { e in
-                e.view.widthAnchor == frameAnchors.widthAnchor - e.insets.horizontal
+            elems.forEach { e in
+                e.view.widthAnchor == frame.widthAnchor - e.insets.horizontal
             }
         }
         
-        switch elements.count {
+        switch elems.count {
         case 1:
-            let e = elements[0]
+            let e = elems[0]
             switch direction {
             case .vertical:
-                e.view.topAnchor == contentAnchros.topAnchor + e.insets.top
-                e.view.bottomAnchor == contentAnchros.bottomAnchor - e.insets.bottom
+                e.view.topAnchor == content.topAnchor + e.insets.top
+                e.view.bottomAnchor == content.bottomAnchor - e.insets.bottom
             case .horizontal:
-                e.view.leftAnchor == contentAnchros.leftAnchor + e.insets.left
-                e.view.rightAnchor == contentAnchros.rightAnchor - e.insets.right
+                e.view.leftAnchor == content.leftAnchor + e.insets.left
+                e.view.rightAnchor == content.rightAnchor - e.insets.right
             }
         default:
-            let f = elements[0] // first
-            let l = elements[elements.count - 1] // last
+            let f = elems[0] // first
+            let l = elems[elems.count - 1] // last
             
             switch direction {
             case .vertical:
-                f.view.topAnchor == contentAnchros.topAnchor + f.insets.top
+                f.view.topAnchor == content.topAnchor + f.insets.top
                 
                 // middle
-                if elements.count > 2 {
-                    for i in 1...elements.count - 2 {
-                        let s = elements[i].insets.top + elements[i - 1].insets.bottom // spacing
-                        elements[i].view.topAnchor == elements[i - 1].view.bottomAnchor + s
+                if elems.count > 2 {
+                    for i in 1...elems.count - 2 {
+                        let s = elems[i].insets.top + elems[i - 1].insets.bottom // spacing
+                        elems[i].view.topAnchor == elems[i - 1].view.bottomAnchor + s
                     }
                 }
                 
-                let ls = l.insets.top + elements[elements.count - 2].insets.bottom // spacing
-                l.view.topAnchor == elements[elements.count - 2].view.bottomAnchor + ls
-                l.view.bottomAnchor == contentAnchros.bottomAnchor - l.insets.bottom
+                let ls = l.insets.top + elems[elems.count - 2].insets.bottom // spacing
+                l.view.topAnchor == elems[elems.count - 2].view.bottomAnchor + ls
+                l.view.bottomAnchor == content.bottomAnchor - l.insets.bottom
             case .horizontal:
-                f.view.leftAnchor == contentAnchros.leftAnchor + f.insets.left
+                f.view.leftAnchor == content.leftAnchor + f.insets.left
                 
                 // middle
-                if elements.count > 2 {
-                    for i in 1...elements.count - 2 {
-                        let s = elements[i].insets.left + elements[i - 1].insets.right // spacing
-                        elements[i].view.leftAnchor == elements[i - 1].view.rightAnchor + s
+                if elems.count > 2 {
+                    for i in 1...elems.count - 2 {
+                        let s = elems[i].insets.left + elems[i - 1].insets.right // spacing
+                        elems[i].view.leftAnchor == elems[i - 1].view.rightAnchor + s
                     }
                 }
                 
-                let ls = l.insets.left + elements[elements.count - 2].insets.right // spacing
-                l.view.leftAnchor == elements[elements.count - 2].view.rightAnchor + ls
-                l.view.rightAnchor == contentAnchros.rightAnchor - l.insets.right
+                let ls = l.insets.left + elems[elems.count - 2].insets.right // spacing
+                l.view.leftAnchor == elems[elems.count - 2].view.rightAnchor + ls
+                l.view.rightAnchor == content.rightAnchor - l.insets.right
             }
         }
     }
@@ -151,5 +144,28 @@ fileprivate extension UIScrollView {
 extension UIScrollView {
     public var container: UIView? {
         Utils.AssociatedObject.get(base: self, key: &containerViewKey)
+    }
+}
+
+// MARK: - Elements
+fileprivate var elementsKey: UInt8 = 0
+
+fileprivate extension UIScrollView {
+    var elements: [Element] {
+        get {
+            Utils.AssociatedObject.get(base: self, key: &elementsKey) { [] }
+        }
+        set {
+            Utils.AssociatedObject.set(base: self, key: &elementsKey, value: newValue)
+        }
+    }
+}
+
+fileprivate extension Array where Element == UIScrollView.Element {
+    mutating func swap(with elems: Self) -> Bool {
+        forEach { $0.view.removeFromSuperview() }
+        removeAll()
+        append(contentsOf: elems)
+        return !elems.isEmpty
     }
 }
